4de0408e81317228f0cdd5a472a78a38585c41ae
David LeGare <excaliburHisSheath@gmail.com>
2015-01-24 12:49:29 -0600

    Live Vote Display - Added display for votes. Votes are tallied every 5 seconds and the count is displayed on the screen.

--- Assets/Scripts/ParseTest.cs
+++ Assets/Scripts/ParseTest.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,17 +10,47 @@ public class ParseTest : MonoBehaviour
 	[Tooltip( "Interval between queries in seconds." )]
 	public float interval;
 
+	public Text upDisplay;
+	public Text downDisplay;
+
+	public String[] voteTypes;
+
+	public Dictionary<String, int> votes;
+
+
+	private bool votesDirty = false;
+
 	private DateTime _lastTime;
 
 	void Awake()
 	{
 		_lastTime = DateTime.UtcNow;
-		Debug.Log( "Now: " + _lastTime );
+		votes = new Dictionary<string,int>();
+		ResetVotes();
 	}
 
 	void Start()
 	{
 		StartCoroutine( QueryVotes() );
+	}
+
+	void Update()
+	{
+		if ( votesDirty )
+		{
+			upDisplay.text = "Up: " + votes["UP"];
+			downDisplay.text = "Down: " + votes["DOWN"];
+			
+			votesDirty = false;
+		}
+	}
+
+	public void ResetVotes()
+	{
+		foreach ( String voteType in voteTypes )
+		{
+			votes[voteType] = 0;
+		}
 	}
 
 	public IEnumerator QueryVotes()
@@ -32,12 +63,15 @@ public class ParseTest : MonoBehaviour
 				.WhereGreaterThan( "createdAt", _lastTime );
 			query.FindAsync().ContinueWith( t =>
 			{
+				ResetVotes();
+
 				IEnumerable<ParseObject> results = t.Result;
 				foreach ( ParseObject vote in results )
 				{
-					Debug.Log( vote );
-					Debug.Log( vote["vote"] );
+					++votes[vote.Get<String>("vote")];
 				}
+
+				votesDirty = true;
 			} );
 
 			_lastTime = DateTime.UtcNow;
