0319afce2f3fe2bd0834e070e6f2059ef0637812
David LeGare <excaliburHisSheath@gmail.com>
2015-01-24 13:46:03 -0600

    Determine Vote Winner - The vote manager spits out an enum for the vote winner when a vote is finished. - Converted the vote type to be an enum rather than strings, since that seemed like a better idea.

--- Assets/Scripts/VoteManager.cs
+++ Assets/Scripts/VoteManager.cs
@@ -5,27 +5,33 @@ using System.Collections;
 using System.Collections.Generic;
 using Parse;
 
-public class ParseTest : MonoBehaviour
+public enum VoteType
+{
+	UP,
+	DOWN,
+	TIE
+}
+
+public class VoteManager : MonoBehaviour
 {
 	[Tooltip( "Interval between queries in seconds." )]
 	public float interval;
 
 	public Text upDisplay;
 	public Text downDisplay;
+	public Text winnerDisplay;
 
-	public String[] voteTypes;
+	[HideInInspector]
+	public VoteType winningVote;
 
-	public Dictionary<String, int> votes;
-
-
+	private Dictionary<VoteType, int> votes;
 	private bool votesDirty = false;
-
 	private DateTime _lastTime;
 
 	void Awake()
 	{
 		_lastTime = DateTime.UtcNow;
-		votes = new Dictionary<string,int>();
+		votes = new Dictionary<VoteType,int>();
 		ResetVotes();
 	}
 
@@ -38,16 +44,17 @@ public class ParseTest : MonoBehaviour
 	{
 		if ( votesDirty )
 		{
-			upDisplay.text = "Up: " + votes["UP"];
-			downDisplay.text = "Down: " + votes["DOWN"];
-			
+			upDisplay.text = "Up: " + votes[VoteType.UP];
+			downDisplay.text = "Down: " + votes[VoteType.DOWN];
+			winnerDisplay.text = "Winner: " + winningVote;
+
 			votesDirty = false;
 		}
 	}
 
 	public void ResetVotes()
 	{
-		foreach ( String voteType in voteTypes )
+		foreach ( VoteType voteType in (VoteType[])Enum.GetValues( typeof( VoteType ) ) )
 		{
 			votes[voteType] = 0;
 		}
@@ -57,18 +64,31 @@ public class ParseTest : MonoBehaviour
 	{
 		while ( true )
 		{
-			Debug.Log( "Querying Parse..." );
-
 			ParseQuery<ParseObject> query = ParseObject.GetQuery( "Vote" )
 				.WhereGreaterThan( "createdAt", _lastTime );
 			query.FindAsync().ContinueWith( t =>
 			{
 				ResetVotes();
 
+				winningVote = VoteType.TIE;
+				int mostVotes = 0;
+
 				IEnumerable<ParseObject> results = t.Result;
 				foreach ( ParseObject vote in results )
 				{
-					++votes[vote.Get<String>("vote")];
+					VoteType voteType = (VoteType)Enum.Parse( typeof( VoteType ), vote.Get<String>("vote") );
+					++votes[voteType];
+
+					int voteCount = votes[voteType];
+					if ( voteCount > mostVotes )
+					{
+						winningVote = voteType;
+						mostVotes = voteCount;
+					}
+					else if ( voteCount == mostVotes )
+					{
+						winningVote = VoteType.TIE;
+					}
 				}
 
 				votesDirty = true;
