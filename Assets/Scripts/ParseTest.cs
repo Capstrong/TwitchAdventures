622de660c025760f114e7fb092d2c8e4e986093a
David LeGare <excaliburHisSheath@gmail.com>
2015-01-24 11:32:51 -0600

    Query Parse At Intervals - At regular intervals the game will query Parse for all entries added since the last query.

--- Assets/Scripts/ParseTest.cs
+++ Assets/Scripts/ParseTest.cs
@@ -1,13 +1,48 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using Parse;
 
 public class ParseTest : MonoBehaviour
 {
+	[Tooltip( "Interval between queries in seconds." )]
+	public float interval;
+
+	private DateTime _lastTime;
+
+	void Awake()
+	{
+		_lastTime = DateTime.UtcNow;
+		Debug.Log( "Now: " + _lastTime );
+	}
+
 	void Start()
 	{
-		ParseObject testObject = new ParseObject("TestObject");
-		testObject["foo"] = "bar";
-		testObject.SaveAsync();
+		StartCoroutine( QueryVotes() );
+	}
+
+	public IEnumerator QueryVotes()
+	{
+		while ( true )
+		{
+			Debug.Log( "Querying Parse..." );
+
+			ParseQuery<ParseObject> query = ParseObject.GetQuery( "Vote" )
+				.WhereGreaterThan( "createdAt", _lastTime );
+			query.FindAsync().ContinueWith( t =>
+			{
+				IEnumerable<ParseObject> results = t.Result;
+				foreach ( ParseObject vote in results )
+				{
+					Debug.Log( vote );
+					Debug.Log( vote["vote"] );
+				}
+			} );
+
+			_lastTime = DateTime.UtcNow;
+
+			yield return new WaitForSeconds( interval );
+		}
 	}
 }
