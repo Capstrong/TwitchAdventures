436bc7dfd12eeff04daa348ab993a423075d4d51
David LeGare <excaliburHisSheath@gmail.com>
2015-01-23 21:06:27 -0600

    Initial Commit - Includes setup Parse things.

---
+++ Assets/Scripts/ParseTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class ParseTest : MonoBehaviour
+{
+	void Start()
+	{
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject["foo"] = "bar";
+		testObject.SaveAsync();
+	}
+}
