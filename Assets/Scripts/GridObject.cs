75e5b4e0b0f1224023409f9e3b9ee5b6d9a1867f
David LeGare <excaliburHisSheath@gmail.com>
2015-01-24 19:47:02 -0600

    Add Village Class - Added a village class for tracking the state of the village. - Changed the GridLocation class into a GridObject class which is a MonoBehaviour. - Made the GridManager into a less shitty singleton.

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridObject : MonoBehaviour
+{
+	public int x;
+	public int y;
+
+	public virtual void Interact( Village village ) { }
+}
