dd65ed9116cd49d09f3aab03fdf9569e66272593
David LeGare <excaliburHisSheath@gmail.com>
2015-01-24 23:50:53 -0600

    Art Integration - Added Wyatt's sprites and setup and configured the animations. - Added a teeny tiny map.

--- Assets/Scripts/Village.cs
+++ Assets/Scripts/Village.cs
@@ -1,7 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Village : MonoBehaviour
 {
-	public int numVillagers;
+	[SerializeField]
+	private int _villagers;
+
+	public int villagers
+	{
+		get
+		{
+			return _villagers;
+		}
+
+		set
+		{
+			_villagers = value;
+			villagersDisplay.text = "Villagers: " + _villagers;
+		}
+	}
+
+	public Text villagersDisplay;
+
+	void Start()
+	{
+		villagersDisplay.text = "Villagers: " + _villagers;
+	}
 }
